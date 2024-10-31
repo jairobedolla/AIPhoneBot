ccdcd02aa9c9b63e9e321e61aae54c9a6b19956d
Jairo Bedolla <jairobedolla@Jairos-MacBook-Pro.local>
2024-10-31 15:07:49 -0700

    Figured out vector embedding with weaviate, still need to work on making it malleable for each user case

--- openaitest.py
+++ openaitest.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 
+from weaviatedb import get_context_for_query, close_client
+
 load_dotenv()
 
 client=OpenAI()
@@ -19,7 +21,7 @@ RELEVANT_INFO = """ H20 Poke grill is is dedicated to serving the freshest ingre
 
 #########
 # Call on Chat-GPT to generate a response based on a templated prompt
-# For future self - implement vector embedding to alleviate
+# For future self - implement vector embedding to alleviate having heavy token usage and better performance
 #########
 
 def generate_response(user_message):
@@ -43,15 +45,55 @@ def generate_response(user_message):
     print("AI: " + response.choices[0].message.content)
     return
 
-if __name__ == '__main__':
+def generate_response_with_vectorstore(user_message, conversation_history):
 
+    context = get_context_for_query("TextFiles", "H20Poke", user_message)
+    c1 = context.objects[0].properties.get("content")
+    c2 = context.objects[1].properties.get("content")
+    c3 = context.objects[2].properties.get("content")
+
+    prompt = f"""
+    You are a helpful assistant working at H20 Poke. These are a few key details 
+    1) {c1} 
+    2) {c2} 
+    3) {c3}
+    
+    Previous conversation:
+    {conversation_history}
+
+    Respond friendly and concisely with the most accurate and relevant information.    
+    """ 
+
+    response = client.chat.completions.create(
+        model= 'gpt-4o-mini', 
+        messages= [{
+            "role": "user",
+            "content": prompt}, 
+            {
+                "role": "user",
+                "content": user_message
+            }])
+    return "AI: " + response.choices[0].message.content
+    
+
+def simulate_conversation():
+    conversation_history = []
     while True:
         user_message = input("Text: ")
         
         if user_message.lower() in EXIT_TERMS:
+            close_client()
             break
 
-        generate_response(user_message)
+        ai_response = generate_response_with_vectorstore(user_message, conversation_history)
+        print(ai_response)
+
+        conversation_history.append({"user_message": user_message, "ai_response": ai_response})
+        if len(conversation_history) > 10:
+            conversation_history.pop(0) 
+
+if __name__ == '__main__':
+    simulate_conversation()
 
 
 
--- weaviatedb.py
+++ weaviatedb.py
@@ -17,34 +17,47 @@ api_key= os.getenv("OPENAI_API_KEY")
 client = weaviate.connect_to_local()
 embeddings = embeddings = OpenAIEmbeddings()
 
-def create_user_knowledge_base(user_id):
-    loader = TextLoader("/Users/jairobedolla/funprojects/AIPhoneBot/H2OPoke.txt")
-    documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=20)
-    docs = text_splitter.split_documents(documents)
+def close_client():
+    client.close()
 
-    db = VectorStore.from_documents(docs, embeddings, client=client, tenant=user_id)
-    return db
+def replace_tenant_data(collection_name= "TextFiles", user_id="H20Poke", filepath="/Users/jairobedolla/funprojects/AIPhoneBot/H2OPoke.txt"):
+    clear_tenant_data(collection_name, user_id)
+    add_data_to_table(collection_name, user_id, filepath)
+
+def clear_tenant_data(collection_name= "TextFiles", user_id="H20Poke"):
+    vs = client.collections.get(collection_name)
+    vs.tenants.remove(user_id) # Removing clears all the data
+    vs.tenants.create(user_id)
 
 def add_tenant_to_table(collection_name= "TextFiles", user_id="H20Poke"):
     textfiles = client.collections.get(collection_name)
     textfiles.tenants.create(user_id)
 
-def add_data_to_table(user_id):
-    textfiles = client.collections.get("TextFiles")
-    tenant = textfiles.with_tenant("user_id")
+def add_data_to_table(collection_name= "TextFiles", user_id="H20Poke", filepath="/Users/jairobedolla/funprojects/AIPhoneBot/H2OPoke.txt"):
+    textfiles = client.collections.get(collection_name)
+    tenant = textfiles.with_tenant(user_id)
 
-    loader = TextLoader("/Users/jairobedolla/funprojects/AIPhoneBot/H2OPoke.txt")
+    loader = TextLoader(filepath)
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=20)
     docs = text_splitter.split_documents(documents)
 
-    db = VectorStore.from_documents(docs, embeddings, client=client, tenant=user_id)
-    return db
+    for doc in docs:
+        tenant.data.insert(
+            properties={
+                "content": doc.page_content
+            }
+        )
 
-
-def get_context_for_query(db, user_id, query):
-    return db.similarity_search(query, k=3, tenant=user_id)
+def get_context_for_query(collection_name, user_id, query):
+    textfiles = client.collections.get(collection_name)
+    tenant = textfiles.with_tenant(user_id)
+    response = tenant.query.near_text(
+        query=query,
+        limit=3
+    )
+    return response
+    
 
 def create_text_schema():
     collection_name = "TextFiles"
@@ -63,36 +76,34 @@ def create_text_schema():
         ],
         multi_tenancy_config=weaviate.classes.config.Configure.multi_tenancy(enabled=True)
     )
-    """text_schema = {
-        "class": "Document",
-        "description": "A class to store documents from a text file with vector embeddings.",
-        "properties": [
-            {
-                "name": "content",
-                "dataType": ["text"],
-                "description": "The main text content of the document.",
-            },
-            {
-                "name": "embedding",
-                "dataType": ["number[]"],
-                "description": "The vector embedding of the document content.",
-                "vectorIndexType": "hnsw",
-                "vectorIndexConfig": {
-                    "efConstruction": 128,
-                    "maxConnections": 64
-                },
-            },
-        ],
-        "vectorizer": "text2vec-openai", 
-        "multiTenancyConfig": {
-            "enabled": True
-        }
-    }
-    client.schema.create_class(text_schema)
-    """
-    print("Document schema created.")
+         
 
 def create_json_schema():
+    collection_name = "QAEntries"
+
+    if client.collections.exists(collection_name):  # In case we've created this collection before
+        client.collections.delete(collection_name)
+
+    client.collections.create(
+        name = collection_name,
+        vectorizer_config= weaviate.classes.config.Configure.Vectorizer.text2vec_openai(),
+        properties=[
+            weaviate.classes.config.Property(
+                name="question",
+                data_type=weaviate.classes.config.DataType.TEXT,
+            ),
+            weaviate.classes.config.Property(
+                name="answer",
+                data_type=weaviate.classes.config.DataType.TEXT,
+            ),
+            weaviate.classes.config.Property(
+                name="category",
+                data_type=weaviate.classes.config.DataType.TEXT,
+            )
+
+        ],
+        multi_tenancy_config=weaviate.classes.config.Configure.multi_tenancy(enabled=True)
+    )
     faq_schema = {
         "class": "FAQ",
         "description": "A class to store FAQ entries from a JSON file with vector embeddings.",
@@ -112,16 +123,6 @@ def create_json_schema():
                 "dataType": ["text"],
                 "description": "The answer part of the FAQ entry.",
             },
-            {
-                "name": "embedding",
-                "dataType": ["number[]"],
-                "description": "The vector embedding of the question-answer pair.",
-                "vectorIndexType": "hnsw",
-                "vectorIndexConfig": {
-                    "efConstruction": 128,
-                    "maxConnections": 64
-                },
-            },
         ],
         "vectorizer": "text2vec-openai",
         "multiTenancyConfig": {
@@ -134,9 +135,19 @@ def create_json_schema():
 
 
 if __name__ == '__main__':
-    multi_collection = client.collections.get("TextFiles")
+    replace_tenant_data()
 
-    # Get a list of tenants
+    client.close()
+
+"""
+multi_collection = client.collections.get("TextFiles")
+    query = input("text: ")
+    context = get_context_for_query("TextFiles", "H20Poke", query)
+
+    for o in context.objects:
+        print(o.properties.get("content"))
+
+# Get a list of tenants
     tenants = multi_collection.tenants.get()
 
     # Iterate through tenants
@@ -146,8 +157,6 @@ if __name__ == '__main__':
         for item in multi_collection.with_tenant(tenant_name).iterator():
             print(f"{tenant_name}: {item.properties}")
     client.close()
-
-"""
 try:
     # Check if the connection is successful
     if client.is_live():
